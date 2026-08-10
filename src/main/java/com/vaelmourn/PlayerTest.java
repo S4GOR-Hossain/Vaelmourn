b07e2944a97332403df93dd2c715dcac0663a649
S4GOR-Hossain <opusagorhossain@gmail.com>
2026-08-10 15:36:45 +0600

    Fix player physics: jump force, CCD, floor collision shape, visual offset

---
+++ src/main/java/com/vaelmourn/PlayerTest.java
@@ -0,0 +1,146 @@
+package com.vaelmourn;
+
+import com.jme3.app.SimpleApplication;
+import com.jme3.bullet.BulletAppState;
+import com.jme3.bullet.collision.shapes.BoxCollisionShape;
+import com.jme3.bullet.control.BetterCharacterControl;
+import com.jme3.bullet.control.RigidBodyControl;
+import com.jme3.input.KeyInput;
+import com.jme3.input.controls.ActionListener;
+import com.jme3.input.controls.KeyTrigger;
+import com.jme3.light.AmbientLight;
+import com.jme3.light.DirectionalLight;
+import com.jme3.material.Material;
+import com.jme3.math.ColorRGBA;
+import com.jme3.math.Vector3f;
+import com.jme3.renderer.Camera;
+import com.jme3.scene.Geometry;
+import com.jme3.scene.Node;
+import com.jme3.scene.shape.Box;
+import com.jme3.scene.shape.Sphere;
+
+public class PlayerTest extends SimpleApplication implements ActionListener {
+
+    private BulletAppState bulletAppState;
+    private BetterCharacterControl playerControl;
+    private Node playerNode;
+
+    private boolean left, right, forward, backward;
+    private final Vector3f walkDirection = new Vector3f();
+    private final float MOVE_SPEED = 6f;
+
+    public static void main(String[] args) {
+        PlayerTest app = new PlayerTest();
+        app.start();
+    }
+
+    @Override
+    public void simpleInitApp() {
+
+        // Physics setup
+        bulletAppState = new BulletAppState();
+        stateManager.attach(bulletAppState);
+
+        // --- FLOOR ---
+        Box floorBox = new Box(50f, 0.5f, 50f);
+        Geometry floor = new Geometry("Floor", floorBox);
+        Material floorMat = new Material(assetManager, "Common/MatDefs/Misc/Unshaded.j3md");
+        floorMat.setColor("Color", ColorRGBA.Gray);
+        floor.setMaterial(floorMat);
+        floor.setLocalTranslation(0, -0.5f, 0);
+
+        BoxCollisionShape floorShape = new BoxCollisionShape(new Vector3f(50f, 0.5f, 50f));
+        RigidBodyControl floorPhysics = new RigidBodyControl(floorShape, 0);
+        floor.addControl(floorPhysics);
+
+        rootNode.attachChild(floor);
+        bulletAppState.getPhysicsSpace().add(floorPhysics);
+
+        // --- LIGHTING ---
+        DirectionalLight sun = new DirectionalLight();
+        sun.setDirection(new Vector3f(-0.5f, -1f, -0.5f).normalizeLocal());
+        rootNode.addLight(sun);
+        AmbientLight ambient = new AmbientLight();
+        ambient.setColor(ColorRGBA.White.mult(0.4f));
+        rootNode.addLight(ambient);
+
+        // --- PLAYER VISUAL (placeholder sphere) ---
+        Sphere playerShape = new Sphere(16, 16, 0.5f);
+        Geometry playerGeom = new Geometry("PlayerVisual", playerShape);
+        Material playerMat = new Material(assetManager, "Common/MatDefs/Misc/Unshaded.j3md");
+        playerMat.setColor("Color", ColorRGBA.Red);
+        playerGeom.setMaterial(playerMat);
+        playerGeom.setLocalTranslation(0, 0.9f, 0);
+
+        playerNode = new Node("Player");
+        playerNode.attachChild(playerGeom);
+        rootNode.attachChild(playerNode);
+
+        // --- PLAYER PHYSICS ---
+        playerControl = new BetterCharacterControl(0.5f, 1.8f, 1f);
+        playerControl.setJumpForce(new Vector3f(0, 8f, 0));
+        playerControl.setGravity(new Vector3f(0, -30f, 0));
+        playerNode.addControl(playerControl);
+        bulletAppState.getPhysicsSpace().add(playerControl);
+
+        // Prevent tunneling through the floor at speed
+        playerControl.getRigidBody().setCcdMotionThreshold(0.1f);
+        playerControl.getRigidBody().setCcdSweptSphereRadius(0.5f);
+
+        // Physics-safe spawn position (do NOT use setLocalTranslation here)
+        playerControl.warp(new Vector3f(0, 5f, 0));
+
+        // --- INPUT ---
+        initKeys();
+
+        // --- CAMERA ---
+        flyCam.setEnabled(false);
+        cam.setLocation(new Vector3f(0, 3, -8));
+    }
+
+    private void initKeys() {
+        inputManager.addMapping("Left", new KeyTrigger(KeyInput.KEY_A));
+        inputManager.addMapping("Right", new KeyTrigger(KeyInput.KEY_D));
+        inputManager.addMapping("Forward", new KeyTrigger(KeyInput.KEY_W));
+        inputManager.addMapping("Backward", new KeyTrigger(KeyInput.KEY_S));
+        inputManager.addMapping("Jump", new KeyTrigger(KeyInput.KEY_SPACE));
+
+        inputManager.addListener(this, "Left", "Right", "Forward", "Backward", "Jump");
+    }
+
+    @Override
+    public void onAction(String name, boolean isPressed, float tpf) {
+        switch (name) {
+            case "Left": left = isPressed; break;
+            case "Right": right = isPressed; break;
+            case "Forward": forward = isPressed; break;
+            case "Backward": backward = isPressed; break;
+            case "Jump":
+                if (isPressed) playerControl.jump();
+                break;
+        }
+    }
+
+    @Override
+    public void simpleUpdate(float tpf) {
+        Camera camera = cam;
+
+        Vector3f camDir = camera.getDirection().clone().setY(0).normalizeLocal();
+        Vector3f camLeft = camera.getLeft().clone().setY(0).normalizeLocal();
+
+        walkDirection.set(0, 0, 0);
+        if (forward) walkDirection.addLocal(camDir);
+        if (backward) walkDirection.addLocal(camDir.negate());
+        if (left) walkDirection.addLocal(camLeft);
+        if (right) walkDirection.addLocal(camLeft.negate());
+
+        walkDirection.normalizeLocal().multLocal(MOVE_SPEED);
+        playerControl.setWalkDirection(walkDirection);
+
+        // Simple follow camera
+        Vector3f playerPos = playerNode.getLocalTranslation();
+        Vector3f camOffset = new Vector3f(0, 3, -8);
+        cam.setLocation(playerPos.add(camOffset));
+        cam.lookAt(playerPos.add(0, 1.5f, 0), Vector3f.UNIT_Y);
+    }
+}
